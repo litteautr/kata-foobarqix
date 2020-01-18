449ed0ca10e73f227106d68c7e95127662ecf4b5
Robin Litteaut <litteautr@gmail.com>
2020-01-18 17:29:00 +0100

    Fix range in Main &=and add comment

--- src/main/java/fr/rli/foobarqix/Main.java
+++ src/main/java/fr/rli/foobarqix/Main.java
@@ -4,10 +4,16 @@ import java.util.stream.IntStream;
 
 public class Main {
 
+    /**
+     * Convert and print conversion result for range of numbers, from 1 to 100.
+     *
+     * @param args no arg needed here
+     * @see README.md for conversion rules.
+     */
     public static void main(String[] args) {
         FooBarQixConverter fbqConverter = new FooBarQixConverter();
 
-        IntStream.range(0, 100).forEach(
+        IntStream.range(1, 101).forEach(
                 input -> System.out.println(fbqConverter.convert(input))
         );
     }
