d8dc2911cc74be2900448f00b0a08b66a55b4479
Robin Litteaut <litteautr@gmail.com>
2020-01-21 10:45:14 +0100

    Refactoring single parameterized test into one test by rule

--- src/test/java/fr/rli/foobarqix/FooBarQixConverterTest.java
+++ src/test/java/fr/rli/foobarqix/FooBarQixConverterTest.java
@@ -2,35 +2,84 @@ package fr.rli.foobarqix;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FooBarQixConverterTest {
+    
+    private static FooBarQixConverter fooBarQixConverter;
 
-    private FooBarQixConverter fooBarQixConverter = new FooBarQixConverter();;
+    @BeforeAll
+    static void setUp() {
+        fooBarQixConverter = new FooBarQixConverter();
+    }
 
-    private static Stream<Arguments> provide_numbers_for_foobarqix_conversion() {
+    @Test
+    public void should_return_input_number_as_string() {
+        assertEquals("1", fooBarQixConverter.convert(1));
+    }
+
+
+    @Test
+    public void should_return_foo_when_divisible_by_3() {
+        assertEquals("Foo", fooBarQixConverter.convert(9));
+    }
+
+    @Test
+    public void should_return_bar_when_divisible_by_5() {
+        assertEquals("Bar", fooBarQixConverter.convert(10));
+    }
+
+    @Test
+    public void should_return_foo_when_contains_3() {
+        assertEquals("Foo", fooBarQixConverter.convert(13));
+    }
+
+    @Test
+    public void should_return_bar_when_contains_5() {
+        assertEquals("Bar", fooBarQixConverter.convert(52));
+    }
+
+    @Test
+    public void should_return_qix_when_contains_7() {
+        assertEquals("Qix", fooBarQixConverter.convert(17));
+    }
+
+    private static Stream<Arguments> provide_numbers_for_divisor_preference() {
         return Stream.of(
-                Arguments.of(1, "1"), // No specific rule for conversion
-                Arguments.of(9, "Foo"), // Divisible by 3
-                Arguments.of(10, "Bar"), // Divisible by 5
-                Arguments.of(60, "FooBar"), // Divisible by 3 and 5
-                Arguments.of(13, "Foo"), // contains 3
-                Arguments.of(25, "BarBar"), // contains 5 and Divisible by 5
-                Arguments.of(17, "Qix"), // contains 7
-                Arguments.of(33, "FooFooFoo"), // Divisible by 3 and contains 3
-                Arguments.of(27, "FooQix"), // Divisible by 3 and contains 7
-                Arguments.of(35, "BarFooBar"), // Divisible by 5 and contains 3 and 5
-                Arguments.of(75, "FooBarQixBar") // Divisible by 5 and 3 and contains 7 and 5
+                Arguments.of("FooBar", 51),
+                Arguments.of("BarFooBar", 35),
+                Arguments.of("FooBarQixBar", 75)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("provide_numbers_for_foobarqix_conversion")
-    public void should_apply_foobarqix_conversion_for_input_number(int inputNumber, String expected) {
-        Assertions.assertEquals(expected, fooBarQixConverter.convert(inputNumber));
+    @MethodSource("provide_numbers_for_divisor_preference")
+    public void should_give_preference_to_divisor(String expected, int input) {
+        assertEquals(expected, fooBarQixConverter.convert(input));
     }
+
+
+    private static Stream<Arguments> provide_numbers_for_preserve_order_analyzing_content() {
+        return Stream.of(
+                Arguments.of("BarFoo", 53),
+                Arguments.of("QixFoo", 73),
+                Arguments.of("FooQix", 37)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provide_numbers_for_preserve_order_analyzing_content")
+    public void should_preserve_order_analyzing_content(String expected, int input) {
+        assertEquals(expected, fooBarQixConverter.convert(input));
+    }
+
+
 }
